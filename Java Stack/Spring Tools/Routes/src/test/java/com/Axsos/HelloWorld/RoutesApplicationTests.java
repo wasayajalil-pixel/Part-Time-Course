d8122e6bb6ebf44d718909cfa4eca6d513e8b72a
Jalil <wasaya.jalil@gmail.com>
2026-06-26 14:52:05 +0300

    routes

---
+++ Java Stack/Spring Tools/Routes/src/test/java/com/Axsos/HelloWorld/RoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Axsos.HelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
