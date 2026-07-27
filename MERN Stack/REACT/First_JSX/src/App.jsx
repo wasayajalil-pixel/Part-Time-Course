c82a7e1b513e64a22d6d8da8a45edb1acd3ea9c1
Jalil <wasaya.jalil@gmail.com>
2026-07-27 13:06:44 +0300

    first jsx

---
+++ MERN Stack/REACT/First_JSX/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <h1>Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
